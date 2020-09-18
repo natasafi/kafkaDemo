6f4fe50e67cd2c3c1a41b1d35cf2fc949c5a8390
Natasa Fragkou <natasha.fragkou@skybettingandgaming.com>
2020-09-18 11:00:44 +0100

    Enables kafka on the main fun

--- src/main/kotlin/com/examples/kafka/demo/Application.kt
+++ src/main/kotlin/com/examples/kafka/demo/Application.kt
@@ -2,10 +2,12 @@ package com.examples.kafka.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
 
+@EnableKafka
 @SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-	runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
