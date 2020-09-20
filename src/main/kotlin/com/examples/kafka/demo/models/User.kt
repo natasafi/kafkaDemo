a8cb23e32fdd5e15e435781a6612b027d9b691f7
Natasa Fragkou <natasha.fragkou@skybettingandgaming.com>
2020-09-20 19:09:03 +0100

    Fixes port erros on yamls

--- src/main/kotlin/com/examples/kafka/demo/models/User.kt
+++ src/main/kotlin/com/examples/kafka/demo/models/User.kt
@@ -1,7 +1,17 @@
 package com.examples.kafka.demo.models
 
+import org.jetbrains.annotations.Nullable
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
 data class User (
+    @Id
     val id: Long,
+    @Column
     val name: String,
+    @Column
+    @Nullable
     val age: Long?
 )
