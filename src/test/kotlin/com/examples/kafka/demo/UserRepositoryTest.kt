105297cb57686fd50262d34616eaf4f429b280f5
Edward Wilson <4864094+ed0906@users.noreply.github.com>
2020-09-28 12:57:51 +0100

    Example Repository Test

--- src/test/kotlin/com/examples/kafka/demo/UserRepositoryTest.kt
+++ src/test/kotlin/com/examples/kafka/demo/UserRepositoryTest.kt
@@ -2,25 +2,26 @@ package com.examples.kafka.demo
 
 import com.examples.kafka.demo.models.createUser
 import com.examples.kafka.demo.repository.UserRepository
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 class UserRepositoryTest @Autowired constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val mongoTemplate: MongoTemplate
 ) {
 
     @BeforeEach
     fun setUpRepository() {
-
         userRepository.deleteAll()
     }
 
@@ -28,9 +29,15 @@ class UserRepositoryTest @Autowired constructor(
     inner class `When accessing GET users endpoint` {
         @Test
         fun `Then should return all users`() {
-            val expectedListOfUsers = listOf(createUserWithIdOf1(), createUserWithIdOf2())
+            // Given
+            mongoTemplate.insert(createUserWithIdOf1())
+            mongoTemplate.insert(createUserWithIdOf2())
 
-            assertEquals(expectedListOfUsers, userRepository.findAll())
+            // When
+            val actualUsers = userRepository.findAll()
+
+            // Then
+            assertThat(actualUsers).isEqualTo(listOf(createUserWithIdOf1(), createUserWithIdOf2()))
         }
     }
 
