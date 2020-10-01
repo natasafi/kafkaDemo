0c1eb8c04075d629949ae146305aba3adb9512eb
Natasa Fragkou <natasha.fragkou@skybettingandgaming.com>
2020-10-01 15:53:37 +0100

    Creates initial kafkaProducerTest class

--- src/test/kotlin/com/examples/kafka/demo/kafka/KafkaProducerTest.kt
+++ src/test/kotlin/com/examples/kafka/demo/kafka/KafkaProducerTest.kt
@@ -1,12 +1,15 @@
 package com.examples.kafka.demo.kafka
 
-import mu.KotlinLogging
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.kafka.listener.KafkaMessageListenerContainer
+import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
@@ -14,15 +17,25 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @ExtendWith(SpringExtension::class)
 @SpringBootTest()
 class KafkaProducerTest {
-    private val logger = KotlinLogging.logger { }
 
     private val TOPIC_NAME = "natasa-topic-example"
 
+    @ExtendWith
+    var embeddedKafkaRule = EmbeddedKafkaRule(1, true, TOPIC_NAME)
+
+    @Autowired
+    private lateinit var embeddedKafkaBroker: EmbeddedKafkaBroker
+
     @MockBean
     private lateinit var container: KafkaMessageListenerContainer<String, String>
+
+    @BeforeAll
+    fun setUp() {
+
 
     @AfterAll
     fun tearDown() {
         container.stop()
     }
+
 }
