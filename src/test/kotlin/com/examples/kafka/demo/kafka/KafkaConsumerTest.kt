a0c3904d9d7cd2551482583947f326574c39d3b0
Edward Wilson <4864094+ed0906@users.noreply.github.com>
2020-10-05 15:16:00 +0100

    Fundamentals of Consumer Test

--- src/test/kotlin/com/examples/kafka/demo/kafka/KafkaConsumerTest.kt
+++ src/test/kotlin/com/examples/kafka/demo/kafka/KafkaConsumerTest.kt
@@ -2,71 +2,47 @@ package com.examples.kafka.demo.kafka
 
 import com.examples.kafka.demo.models.User
 import com.examples.kafka.demo.repository.UserRepository
-import mu.KotlinLogging
-import org.assertj.core.api.Assertions
-import org.mockito.BDDMockito.given
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.Import
-import org.springframework.kafka.annotation.KafkaHandler
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.test.context.EmbeddedKafka
-import org.springframework.stereotype.Component
 import org.springframework.test.context.junit.jupiter.SpringExtension
-
-private val logger = KotlinLogging.logger { }
+import java.util.concurrent.CountDownLatch
 
 @EmbeddedKafka(ports = [9093], topics = ["natasa-topic-example"])
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
-@Import(KafkaConsumerTest.SomeProducer::class)
 class KafkaConsumerTest {
-    @Autowired
-    private lateinit var consumer: KafkaConsumer
 
+    // Inject a real Kafka Template so we can publish messages to Kafka
     @Autowired
-    private lateinit var someProducer: SomeProducer
+    private lateinit var kafkaTemplate: KafkaTemplate<String, User>
 
+    // Inject a mock UserRepository that will be used by Spring
     @MockBean
     private lateinit var userRepository: UserRepository
 
     @Test
     fun consumeMessage() {
         // Given
+        val latch = CountDownLatch(1) // Create a latch that expects to have its countDown method called once
         val user = User("id", "name", 20)
-
-        given(userRepository.save(any()).willReturn(user)
+        given(userRepository.save(any<User>())).will { // Every time userRepository.save(...) is called this block of code will run
+            latch.countDown() // Count down the latch
+            user // Return the user
+        }
 
         // When
-        consumer.consumeMessage(user)
+        kafkaTemplate.send("natasa-topic-example", user) // Publish a message to Kafka
 
         // Then
-        val messageConsumed: User = someProducer.waitForMessage()
-        Assertions.assertThat(messageConsumed).isEqualTo(user)
-    }
-
-    @Component
-    class SomeProducer(private val kafkaTemplate: KafkaTemplate<String, User>) {
-
-        private val messages = mutableListOf<User>()
-
-        @KafkaHandler
-        fun producer(user: User) {
-            logger.info { "Your message is produced:[$user]" }
-            kafkaTemplate.sendDefault(user)
-        }
-
-        fun waitForMessage(): User {
-            var counter = 0
-            do {
-                if (messages.isNotEmpty()) return messages[0]
-                else Thread.sleep(1000)
-                counter++
-            } while (counter < 10)
-            throw AssertionError("No message was received")
-        }
+        latch.await() // wait for the latch to be counted down by something calling userRepository.save()
+        verify(userRepository).save(user) // Verify the repository was called with the right user
     }
 }
