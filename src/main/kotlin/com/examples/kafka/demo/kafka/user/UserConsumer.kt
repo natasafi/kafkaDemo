e149a3f56cf9ff874e0d6e63ce7f0b5605187465
Natasa Fragkou <natasha.fragkou@skybettingandgaming.com>
2020-10-08 17:08:10 +0100

    Replaces class arguement with consumerRecord

---
+++ src/main/kotlin/com/examples/kafka/demo/kafka/user/UserConsumer.kt
@@ -0,0 +1,23 @@
+package com.examples.kafka.demo.kafka.user
+
+import com.examples.kafka.demo.models.User
+import com.examples.kafka.demo.repository.UserRepository
+import mu.KotlinLogging
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+private val logger = KotlinLogging.logger { }
+
+
+@Component
+class UserConsumer(val userRepository: UserRepository) {
+
+    @KafkaListener(topics = ["natasa-topic-example"], groupId = "natasa-message-consumer")
+    fun consumeMessage(record: ConsumerRecord<String, User>) {
+
+        logger.info { "Your user is safe with me: ${record.value()} with key ${record.key()}" }
+
+        userRepository.save(record.value())
+    }
+}
