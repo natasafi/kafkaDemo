ac35433c842affb77c94c57c17971b3e0e79d173
Natasa Fragkou <natasha.fragkou@skybettingandgaming.com>
2020-10-15 13:54:37 +0100

    Adds email and password field for User

--- src/main/kotlin/com/examples/kafka/demo/models/User.kt
+++ src/main/kotlin/com/examples/kafka/demo/models/User.kt
@@ -1,6 +1,8 @@
 package com.examples.kafka.demo.models
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.IndexDirection
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "user")
@@ -9,5 +11,8 @@ data class User(
     val id: String,
     val name: String,
     val age: Int?,
-    val address: Address? = null
+    val address: Address? = null,
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING, dropDups = true)
+    val email: String,
+    val password: String
 )
