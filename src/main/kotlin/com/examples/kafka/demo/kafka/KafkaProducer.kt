18e05e03bd22952c23e8c5b3edaa9908094645ae
Natasa Fragkou <natasha.fragkou@skybettingandgaming.com>
2020-10-19 15:25:26 +0100

    Adds encoding to producer

--- src/main/kotlin/com/examples/kafka/demo/kafka/KafkaProducer.kt
+++ src/main/kotlin/com/examples/kafka/demo/kafka/KafkaProducer.kt
@@ -2,6 +2,7 @@ package com.examples.kafka.demo.kafka
 
 import com.examples.kafka.demo.models.Address
 import com.examples.kafka.demo.models.User
+import com.examples.kafka.demo.security.PasswordEncoder
 import mu.KotlinLogging
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
@@ -13,17 +14,20 @@ private const val ADDRESS_NAME: String = "address-topic"
 @Service
 class KafkaProducer(
     private val userTemplate: KafkaTemplate<String, User>,
-    private val addressTemplate: KafkaTemplate<String, Address>
+    private val addressTemplate: KafkaTemplate<String, Address>,
+    private val encoder: PasswordEncoder
 ) {
 
     fun produceUser(user: User) {
+        val hashedPassword = encoder.hashPassword(user.password)
+
         logger.info { "Your message is being produced: $user" }
-        userTemplate.send(USERS_NAME, user.id, user)
+
+        userTemplate.send(USERS_NAME, user.id, user.copy(password = hashedPassword))
     }
 
     fun produceAddress(userId: String, address: Address) {
         logger.info { "Your address is being produced: $address with $userId" }
         addressTemplate.send(ADDRESS_NAME, userId, address)
     }
-
 }
